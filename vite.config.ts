bf294931dc7928acea223eeb1b349c28ba8bbf03
gupta-niharika <sanikchar@gmail.com>
2025-11-09 16:57:42 +0530

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", // 👈 important for USER GitHub Pages (not project)
+  base: "/gupta-niharika.github.io/",
   server: {
     host: "::",
     port: 8080,
